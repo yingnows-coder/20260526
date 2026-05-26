0860610304eb774cc61f6505a5a1b15cb4cff4b8
yingnows-coder <yingnows@gmail.com>
2026-05-26 21:08:46 +0800

    Update aa.py

--- aa.py
+++ aa.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
-from streamlit_sortables import sort_items
 
 # ==========================================
 # 基本設定
